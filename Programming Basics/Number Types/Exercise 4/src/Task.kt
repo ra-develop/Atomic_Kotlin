3ceed3b5298d260ab8a38556c43b697713dcdd67
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-01 22:25:07 +0100

    Atomic Kotlin: Programming Basics: numberTypesExercise4 finished

--- Programming Basics/Number Types/Exercise 4/src/Task.kt
+++ Programming Basics/Number Types/Exercise 4/src/Task.kt
@@ -2,7 +2,7 @@
 package numberTypesExercise4
 
 fun convertToMilliseconds(hours: Int, minutes: Int, seconds: Int): Long =
-  TODO()
+  (((hours * 60L) + minutes) * 60L + seconds) * 1_000L
 
 fun main() {
   println(convertToMilliseconds(1, 30, 0))
