53664902b3e3a5f13320bbd60fc6124dfc408a5b
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-01 16:24:57 +0100

    Atomic Kotlin: ifExpressionsExercise1 finished

--- Programming Basics/if Expressions/Exercise 1/src/Task.kt
+++ Programming Basics/if Expressions/Exercise 1/src/Task.kt
@@ -2,7 +2,7 @@
 package ifExpressionsExercise1
 
 fun checkSign(number: Int): String =
-  TODO()
+  if (number == 0) "zero" else if (number < 0) "negative" else "positive"
 
 fun main() {
   println(checkSign(-19))  // negative
