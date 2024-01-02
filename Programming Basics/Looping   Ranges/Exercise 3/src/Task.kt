1a0d5c454d9b72e27b2a22b2ef16f3062f3fc97b
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-02 13:42:46 +0100

    Atomic Kotlin: Looping and Ranges: loopingAndRangesExercise3 finished

--- Programming Basics/Looping   Ranges/Exercise 3/src/Task.kt
+++ Programming Basics/Looping   Ranges/Exercise 3/src/Task.kt
@@ -2,7 +2,11 @@
 package loopingAndRangesExercise3
 
 fun sumOfEven(n: Int): Int {
-  TODO()
+  var result = 0
+  for (i in 1 .. n) {
+    if (i % 2 == 0) result += i
+  }
+  return result
 }
 
 fun main() {
