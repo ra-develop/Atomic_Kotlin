857587b31d568695b826118280b8e66a847aa93d
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-03 00:40:04 +0100

    Atomic Kotlin: The in Keyword: theInKeywordExercise4 finished

--- Programming Basics/The in Keyword/Exercise 4/src/Task.kt
+++ Programming Basics/The in Keyword/Exercise 4/src/Task.kt
@@ -1,10 +1,20 @@
 // InKeyword/Task4.kt
 package theInKeywordExercise4
 
-// isValidCharacter
+fun isValidCharacter(char: Char): Boolean =
+  char in 'a' .. 'z' || char in 'A' .. 'Z'
+          || char in '0' .. '9' || char == '_'
 
 fun isValidIdentifier(s: String): Boolean {
-  TODO()
+  if (s.isEmpty() || s[0] in '0' .. '9') {
+    return false
+  }
+  for (i in 0 .. s.lastIndex) {
+    if (!isValidCharacter(s[i])) {
+      return false
+    }
+  }
+  return true
 }
 
 fun main() {
