0badf700644bc023802e0a0d9c5ab986a9bd8a1a
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-02 11:03:01 +0100

    Atomic Kotlin: Booleans: booleansExercise2 finished

--- Programming Basics/Booleans/Exercise 2/src/Task.kt
+++ Programming Basics/Booleans/Exercise 2/src/Task.kt
@@ -1,12 +1,23 @@
 // Booleans/Task2.kt
 package booleansExercise2
 
-fun showAnd() {}
+fun showAnd(first: Boolean, second: Boolean) {
+  println("$first && $second == ${first && second}")
+}
 
-fun showOr() {}
+fun showOr(first: Boolean, second: Boolean) {
+  println("$first || $second == ${first || second}")
+}
 
 fun showTruthTable() {
-  TODO()
+  showAnd(true, true)
+  showAnd(true, false)
+  showAnd(false, false)
+  showAnd(false, true)
+  showOr(true, true)
+  showOr(true, false)
+  showOr(false, false)
+  showOr(false, true)
 }
 
 fun main() {
