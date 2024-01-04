54ff7cdacb524594e44464a7922e68eb4202b68c
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-04 14:55:09 +0100

    Atomic Kotlin: Summary 1: summaryIExercise8 finished

--- Programming Basics/Summary 1/Exercise 8/src/Task.kt
+++ Programming Basics/Summary 1/Exercise 8/src/Task.kt
@@ -2,9 +2,26 @@
 package summaryIExercise8
 
 fun reverseDecimal(number: Int): Int {
-  TODO()
+  var worker = number
+  var result = 0
+  while (worker > 0) {
+    result *= 10
+    result += worker % 10
+    worker /= 10
+  }
+  return result
 }
 
 fun main() {
-  println(reverseDecimal(1234))  // 4321
+//  var worker = 1234
+//  var result = 0
+//
+//    val t = worker % 10
+//    result += t
+//    result *= 10
+//    worker /= 10
+//  println(result)
+//
+//  println(worker)
+  println(reverseDecimal(123456789))  // 4321
 }
