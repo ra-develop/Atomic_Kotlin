b71644f5cb5f0da3c2f73c69d2cb1765bd54c1aa
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-06 22:57:04 +0100

    Atomic Kotlin: Introduction to Objects: Constraining Visibility: constrainingVisibilityExercise1 finished

--- Introduction to Objects/Constraining Visibility/Exercise 1/src/Task.kt
+++ Introduction to Objects/Constraining Visibility/Exercise 1/src/Task.kt
@@ -1,10 +1,26 @@
 // Visibility/Task1.kt
 package constrainingVisibilityExercise1
 
-// TODO
+class Alien( val name: String,  val species: String, private var planet: String) {
+  fun movePlanet(to: String) {
+    planet = to
+  }
+
+  override fun toString(): String {
+    return "Alien $name, $species: $planet"
+  }
+}
 
 fun main() {
-  // TODO
+  val a1 = Alien("Arthricia", "Cat Person", "PurgePlanet")
+  println(a1)
+  a1.movePlanet("Earth C-137")
+  println(a1)
+
+  val a2 = Alien("Dale", "Giant", "Gearworld")
+  println(a2)
+  a2.movePlanet("Parblesnops")
+  println(a2)
 }
 /* Expected output:
 Alien Arthricia, Cat Person: PurgePlanet
