e9bdd10d6001a367bd9f0602a24524363fe46661
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-07 22:36:09 +0100

    Atomic Kotlin: Introduction to Objects: Exceptions: exceptionsExercise1 finished

--- Introduction to Objects/Exceptions/Exercise 1/src/Task.kt
+++ Introduction to Objects/Exceptions/Exercise 1/src/Task.kt
@@ -2,7 +2,12 @@
 package exceptionsExercise1
 
 fun main() {
-  println("Can't be converted to double: ???")
+  println("Can't be converted to double: 1,2")
 
-  // TODO
+//  val i = "12".toDouble()
+//  val i = "1.2".toDouble()
+//  val i = "1,2".toDouble()
+//  val i = "1.2e0".toDouble()
+//  val i = "1.2e1".toDouble()
+//  val i = "1.2e-1".toDouble()
 }
