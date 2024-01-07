635b279e09ad5f21795aed458495e9fa58eca0ef
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-07 20:40:54 +0100

    Atomic Kotlin: Introduction to Objects: Testing: testingExercise2 finished

--- Introduction to Objects/Testing/Exercise 2/src/Task.kt
+++ Introduction to Objects/Testing/Exercise 2/src/Task.kt
@@ -3,8 +3,12 @@ package testingExercise2
 import atomictest.eq
 import atomictest.neq
 
-fun sum(a: Int, b: Int, c: Int): Int = TODO()
+fun sum(a: Int, b: Int, c: Int): Int = a + b + c
 
 fun main() {
-  TODO()
+  sum(1,2,3) eq 6
+  sum(4,5,6) eq 15
+  sum(-1,-2,-3) eq -6
+  sum(-4,5,-6) eq -5
+  sum(0,0,1,) neq 0
 }
