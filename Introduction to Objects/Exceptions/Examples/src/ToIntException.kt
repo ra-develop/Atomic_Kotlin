a84847bdcd263ac9b2afc469de167bb2240b253f
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-07 22:02:09 +0100

    Atomic Kotlin: Introduction to Objects: Exceptions: Examples finished

--- Introduction to Objects/Exceptions/Examples/src/ToIntException.kt
+++ Introduction to Objects/Exceptions/Examples/src/ToIntException.kt
@@ -3,7 +3,7 @@ package exceptions
 
 fun erroneousCode() {
   // Uncomment this line to get an exception:
-  // val i = "1$".toInt()        // [1]
+   val i = "1$".toInt()        // [1]
 }
 
 fun main() {
