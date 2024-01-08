71be67cee68e66095be8a6c22f2cfa746054825d
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-08 18:13:29 +0100

    Atomic Kotlin: Introduction to Objects: Variable Argument Lists: variableArgumentListsExercise2 finished

--- Introduction to Objects/Variable Argument Lists/Exercise 2/src/Task.kt
+++ Introduction to Objects/Variable Argument Lists/Exercise 2/src/Task.kt
@@ -1,10 +1,12 @@
 // Varargs/Task2.kt
 package variableArgumentListsExercise2
 
-fun printArgs() {}
+fun printArgs(s: String, vararg ints: Int) {
+  println("$s${ints.asList()}")
+}
 
 fun main() {
-//  printArgs("Numbers: ", 1, 2, 3)
+  printArgs("Numbers: ", 1, 2, 3)
 }
 /* Expected output:
 Numbers: [1, 2, 3]
