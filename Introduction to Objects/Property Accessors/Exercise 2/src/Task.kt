e4c4262dc2c75def5c7ed4862b70b6b06db12a9c
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-10 13:52:56 +0100

    Atomic Kotlin: Introduction to Objects: Property Accessors: propertyAccessorsExercise2 finished

--- Introduction to Objects/Property Accessors/Exercise 2/src/Task.kt
+++ Introduction to Objects/Property Accessors/Exercise 2/src/Task.kt
@@ -2,14 +2,28 @@
 package propertyAccessorsExercise2
 import atomictest.eq
 
-// TODO: create class Accessible
+class Accessible {
+  var mutable: String = ""
+    set(value) {
+      println("mutable:set")
+      field = value
+    }
+    get() {
+      println("mutable:get")
+      return field
+    }
+
+  val readOnly: String
+    get() {
+      println("readOnly:get")
+      return mutable
+    }
+}
 
 fun main() {
-/*
   val accessible = Accessible()
   accessible.mutable = "abc"
   accessible.readOnly eq "abc"
-*/
 }
 /* Expected output:
 mutable:set
