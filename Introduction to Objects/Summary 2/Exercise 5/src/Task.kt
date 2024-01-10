2d91c28590389703222425cd5f18bfe4563583ef
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-10 17:10:27 +0100

    Atomic Kotlin: Introduction to Objects: Summary 2: summaryIIExercise5 finished

--- Introduction to Objects/Summary 2/Exercise 5/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 5/src/Task.kt
@@ -1,15 +1,21 @@
 // Summary2/Task5.kt
 package summaryIIExercise5
 
-fun va1() {}
+fun va1(vararg intArgs: Int) {
+  println("va1:")
+  for (arg in intArgs) {
+    println(arg)
+  }
+}
 
-fun va2() {}
+fun va2(vararg intArgs: Int) {
+  println("va2:")
+  va1(*intArgs)
+}
 
 fun main() {
-/*
   va1(1, 2, 3)
   va2(4, 5, 6)
-*/
 }
 /* Expected output:
 va1:
