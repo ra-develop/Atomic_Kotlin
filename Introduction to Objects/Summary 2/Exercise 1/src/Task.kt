b02b14c7144bcb48ffa2a0a4564528853bdb7bcd
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-10 15:43:41 +0100

    Atomic Kotlin: Introduction to Objects: Summary 2: summaryIIExercise1 finished

--- Introduction to Objects/Summary 2/Exercise 1/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 1/src/Task.kt
@@ -2,8 +2,16 @@
 package summaryIIExercise1
 import atomictest.eq
 
-// add Boring class
+class Boring {
+  fun a() = 1.618
+  fun b() = "ratio"
+  fun c() = 11
+}
 
 fun main() {
-  // test Boring
+  val boring = Boring()
+
+  boring.a() eq 1.618
+  boring.b() eq "ratio"
+  boring.c() eq 11
 }
