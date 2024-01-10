cda9a653b5973b19f4f214a3ef3a9080ec40a0ac
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-10 13:59:47 +0100

    Atomic Kotlin: Introduction to Objects: Property Accessors: propertyAccessorsExercise3 finished

--- Introduction to Objects/Property Accessors/Exercise 3/src/Task.kt
+++ Introduction to Objects/Property Accessors/Exercise 3/src/Task.kt
@@ -2,15 +2,22 @@
 package propertyAccessorsExercise3
 import atomictest.eq
 
-// TODO: create class MessageStorage
+class MessageStorage {
+  private val _messages: MutableList<String> = mutableListOf()
+
+  val messages: List<String>
+    get() = _messages
+
+  fun addMessage(message: String) {
+    _messages.add(message)
+  }
+}
 
 fun main() {
-/*
   val messageStorage = MessageStorage()
   messageStorage.addMessage("first")
   messageStorage.addMessage("second")
   messageStorage.messages eq listOf("first", "second")
   // doesn't compile:
 //  messageStorage.messages.remove("first")
-*/
 }
