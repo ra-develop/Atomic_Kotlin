ccc4b2030a84ab41b22a2a6dafb67f2b37ad1b3c
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-11 18:22:22 +0100

    Atomic Kotlin: Usability: Named & when Expressions: whenExpressionsExercise2 finished

--- Usability/when Expressions/Exercise 2/src/Task.kt
+++ Usability/when Expressions/Exercise 2/src/Task.kt
@@ -3,7 +3,13 @@ package whenExpressionsExercise2
 import atomictest.eq
 
 fun temperature(temperature: Int): String =
-  TODO()
+  when {
+    temperature < -5 -> "Freezing"
+    temperature in -5..4 -> "Cold"
+    temperature in 5..14 -> "Cool"
+    temperature in 15..24 -> "Warm"
+    else -> "Hot"
+  }
 
 fun main() {
   temperature(30) eq "Hot"
