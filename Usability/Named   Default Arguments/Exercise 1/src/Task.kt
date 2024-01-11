197ff51863f856793a3d37631435e808877e3da3
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-11 14:27:16 +0100

    Atomic Kotlin: Usability: Named & Default Arguments: namedAndDefaultArgumentsExercise1 finished

--- Usability/Named   Default Arguments/Exercise 1/src/Task.kt
+++ Usability/Named   Default Arguments/Exercise 1/src/Task.kt
@@ -1,10 +1,19 @@
 // NamedAndDefaultArgs/Task1.kt
 package namedAndDefaultArgumentsExercise1
 
-class Rectangle
+class Rectangle (
+  val side1: Double = 1.0,
+  val side2: Double = 2.0,
+  val color: String = "yellow"
+) {
+  override fun toString(): String {
+    return "$color rectangle $side1 x $side2"
+  }
+}
 
 fun main() {
   println(Rectangle())
 
-  // TODO
+  println(Rectangle(color = "black"))
+  println(Rectangle(5.0,5.0))
 }
