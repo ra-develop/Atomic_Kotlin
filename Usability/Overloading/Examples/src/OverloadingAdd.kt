437eb59bcb935121de88f6193ff265976ce3ff7b
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-11 17:07:37 +0100

    Atomic Kotlin: Usability: Named & Overloading: Examples finished

--- Usability/Overloading/Examples/src/OverloadingAdd.kt
+++ Usability/Overloading/Examples/src/OverloadingAdd.kt
@@ -5,11 +5,16 @@ import atomictest.eq
 fun addInt(i: Int, j: Int) = i + j
 fun addDouble(i: Double, j: Double) = i + j
 
+fun addString(i: String, j: String) = i + j
+
 fun add(i: Int, j: Int) = i + j
+fun add(i: String, j: String) = i + j
 fun add(i: Double, j: Double) = i + j
 
 fun main() {
   addInt(5, 6) eq add(5, 6)
   addDouble(56.23, 44.77) eq
     add(56.23, 44.77)
+  addString("56.23", "44.77") eq
+          add("56.23", "44.77")
 }
