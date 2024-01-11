81b4eef046ab982b0baaadea94c773d6b868864d
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-12 00:18:05 +0100

    Atomic Kotlin: Usability: Named & when Expressions: whenExpressionsExercise3 finished

--- Usability/when Expressions/Exercise 3/src/Task.kt
+++ Usability/when Expressions/Exercise 3/src/Task.kt
@@ -3,9 +3,22 @@ package whenExpressionsExercise3
 import atomictest.eq
 
 fun balanced(input: String): Boolean {
-  TODO()}
+  if (input.first() == ')') return false
+  var counterLeftParentheses = 0
+  for (ch in input) {
+    when(ch) {
+      '(' -> counterLeftParentheses++
+      ')' -> counterLeftParentheses--
+      ' ' -> {}
+      else -> throw IllegalArgumentException()
+    }
+  }
+  if (counterLeftParentheses != 0) return false
+  return true
+}
 
 fun main() {
   balanced("(()) ()") eq true
   balanced(")(") eq false
+  balanced("((()") eq false
 }
