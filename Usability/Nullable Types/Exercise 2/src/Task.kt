b95d99e22e186e8c65304485124ff7478bd173b8
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-13 18:08:05 +0100

    Atomic Kotlin: Usability: Nullable Types: nullableTypesExercise2 finished

--- Usability/Nullable Types/Exercise 2/src/Task.kt
+++ Usability/Nullable Types/Exercise 2/src/Task.kt
@@ -1,10 +1,14 @@
 // NullableTypes/Task2.kt
 package nullableTypesExercise2
 
-// fun zilch()
-// fun zilch2()
+ fun zilch(): String? {
+   return null
+ }
+ fun zilch2(): String? {
+   return zilch()
+ }
 
 fun main() {
-  // var x
-  // var y
+   var x = zilch()
+   var y = zilch2()
 }
