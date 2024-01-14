d04e64a06971964861fe62f9f1e9c8ef23787780
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-14 11:53:40 +0100

    Atomic Kotlin: Usability: Safe Calls & the Elvis Operator: safeCallsAndTheElvisOperatorExercise1 finished

--- Usability/Safe Calls   the Elvis Operator/Exercise 1/src/Task.kt
+++ Usability/Safe Calls   the Elvis Operator/Exercise 1/src/Task.kt
@@ -2,11 +2,11 @@
 package safeCallsAndTheElvisOperatorExercise1
 import atomictest.eq
 
-fun downcase() {}
+fun downcase(s: String?): String {
+  return s?.lowercase() ?: ""
+}
 
 fun main() {
-/*
   downcase(null) eq ""
   downcase("ABC") eq "abc"
-*/
 }
