4ade7e05bacb8cfb332157b069ea1d4be3179da5
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-15 12:16:49 +0100

    Atomic Kotlin: Usability: Introduction to Generics: introductionToGenericsExercise3 finished

--- Usability/Introduction to Generics/Exercise 3/src/Task.kt
+++ Usability/Introduction to Generics/Exercise 3/src/Task.kt
@@ -2,10 +2,17 @@
 package introductionToGenericsExercise3
 import atomictest.eq
 
-class CountingSet
+class CountingSet<E> {
+  private val container = mutableMapOf<E, Int>()
+  fun add(element: E) {
+      container[element] = (container[element] ?: 0) + 1
+  }
+  fun count(element: E) = container[element] ?: 0
+
+  fun toSet() = container.keys.toSet()
+}
 
 fun main() {
-/*
   val cs = CountingSet<String>()
   cs.add("abc")
   cs.add("abc")
@@ -13,5 +20,5 @@ fun main() {
   cs.count("abc") eq 2
   cs.count("def") eq 1
   cs.count("xyz") eq 0
-*/
+  cs.toSet() eq setOf("abc", "def")
 }
