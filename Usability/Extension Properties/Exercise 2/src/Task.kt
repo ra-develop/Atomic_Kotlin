1dee47aa5a94cdb271cf33b1131c4fe42dfe5428
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-15 13:01:46 +0100

    Atomic Kotlin: Usability: Extension Properties: extensionPropertiesExercise2 finished

--- Usability/Extension Properties/Exercise 2/src/Task.kt
+++ Usability/Extension Properties/Exercise 2/src/Task.kt
@@ -2,11 +2,16 @@
 package extensionPropertiesExercise2
 import atomictest.eq
 
-// TODO: implement 'reversed'
+val <T> List<T>.reversed: List<T>
+  get() {
+    val result = mutableListOf<T>()
+    for (i in lastIndex downTo 0) {
+      result += get(i)
+    }
+    return result
+  }
 
 fun main() {
-/*
   val list = listOf(1, 2, 3)
   list.reversed eq listOf(3, 2, 1)
-*/
 }
