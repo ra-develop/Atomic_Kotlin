181cc0e3a90d7b17fe69360318c6dd60478bcfb4
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-15 11:39:56 +0100

    Atomic Kotlin: Usability: Introduction to Generics: introductionToGenericsExercise2 finished

--- Usability/Introduction to Generics/Exercise 2/src/Task.kt
+++ Usability/Introduction to Generics/Exercise 2/src/Task.kt
@@ -4,11 +4,15 @@ import atomictest.eq
 
 fun <T> List<T>.reversed(): List<T> {
   val result = mutableListOf<T>()
-  TODO()
+  for (i in lastIndex downTo  0 ) {
+    result.add(this[i])
+  }
   return result
 }
 
 fun main() {
   val list = listOf(1, 2, 3)
   list.reversed() eq listOf(3, 2, 1)
+  val list1 = listOf<Int>()
+  list1.reversed() eq listOf()
 }
