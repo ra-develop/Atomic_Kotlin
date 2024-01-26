b8873ad9a8ed23cdc04664e00fdb8766e7c98a5f
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-26 14:49:56 +0100

    Atomic Kotlin: Usability: Member References:  memberReferencesExercise3 finished

--- Functional Programming/Member References/Exercise 3/src/Task.kt
+++ Functional Programming/Member References/Exercise 3/src/Task.kt
@@ -8,28 +8,35 @@ val Int.isEven: Boolean
 // Returns all the divisors of the number
 // except 1 and the number itself
 private fun getNonTrivialDivisors(num: Int): List<Int> {
-  TODO()
+  var result = mutableListOf<Int>()
+  for (divider in 2 until num) {
+    if (num % divider == 0) result.add(divider)
+  }
+  return result
 }
 
 fun Int.isPrime(): Boolean {
-  TODO()
+  return getNonTrivialDivisors(this).isEmpty() && this != 1
 }
 
 fun isPerfect(num: Int): Boolean {
-  TODO()
+  if (num == 1) return false
+  val notTrivialDivisors = getNonTrivialDivisors(num)
+  val sumOfDivisors = notTrivialDivisors.sum() + 1
+  return num == sumOfDivisors
 }
 
 fun main() {
   val range = 1..1000
-  val even = range.filter(TODO())
+  val even = range.filter(Int::isEven)
   even.size eq 500
 
-  val prime = range.filter(TODO())
+  val prime = range.filter(Int::isPrime)
   prime.take(7) eq listOf(2, 3, 5, 7, 11, 13, 17)
   prime.takeLast(7) eq
     listOf(953, 967, 971, 977, 983, 991, 997)
 
-  val perfect = range.filter(TODO())
+  val perfect = range.filter(::isPerfect)
   perfect eq listOf(6, 28, 496)
   // 6 = 1 + 2 + 3
   // 28 = 1 + 2 + 4 + 7 + 14
