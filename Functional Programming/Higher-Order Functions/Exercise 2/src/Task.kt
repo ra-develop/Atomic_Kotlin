12a8b422ceb64d5e6c9ad1dd37deae68334c9f50
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-01-28 20:40:14 +0100

    Atomic Kotlin: Usability: Higher-Order Functions:  higherOrderFunctionsExercise2 finished

--- Functional Programming/Higher-Order Functions/Exercise 2/src/Task.kt
+++ Functional Programming/Higher-Order Functions/Exercise 2/src/Task.kt
@@ -6,7 +6,9 @@ fun List<Int>.transformVersion1(): List<Int> =
   filter { it % 2 == 0 }.map { it * it }
 
 fun List<Int>.transformVersion2(): List<Int> =
-  mapNotNull { TODO() }
+  mapNotNull {
+    if (it % 2 == 0) { it * it } else { null }
+  }
 
 fun main() {
   val list = listOf(1, 2, 3)
