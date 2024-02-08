ad11f185e071568638df3a2f7d36aad697472aa7
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-02-08 16:45:29 +0100

    Atomic Kotlin: Usability: Sequences:  sequencesExercise2 finished

--- Functional Programming/Sequences/Exercise 2/src/Task.kt
+++ Functional Programming/Sequences/Exercise 2/src/Task.kt
@@ -4,11 +4,19 @@ package sequencesExercise2
 fun School.studentInstructors(
   student: Student
 ): Set<Instructor> {
-  TODO()
+  return lessons.filter {
+    student in it.students
+  }.mapTo(mutableSetOf()) {
+    it.instructor
+  }
 }
 
 fun School.studentsOf(
   instructor: Instructor
 ): Set<Student> {
-  TODO()
+  return lessons.filter {
+    instructor == it.instructor
+  }.flatMapTo(mutableSetOf()) {
+    it.students.asSequence()
+  }
 }
