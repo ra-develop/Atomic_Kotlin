328eeffc05116aff9855d8743353b0be549bcb79
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-02-11 20:04:21 +0100

    Atomic Kotlin: Usability: Sequences:  sequencesExercise3 finished

--- Functional Programming/Sequences/Exercise 3/src/Task.kt
+++ Functional Programming/Sequences/Exercise 3/src/Task.kt
@@ -1,5 +1,9 @@
 // Sequences/Task3.kt
 package sequencesExercise3
 
-fun School.averageInstructorRating(instructor: Instructor): Double =
-  TODO()
+fun School.averageInstructorRating(instructor: Instructor): Double  =
+  lessons.filter {
+    it.instructor == instructor
+  }.flatMap {
+    it.rating.values.asSequence()
+  }.average()
